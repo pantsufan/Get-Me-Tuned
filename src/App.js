81c24293514b847740d7ad9000d217cf8a0a539b
pantsufan <joel28@protonmail.com>
2021-01-30 11:18:00 +0530

    Fixed Favicon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-import ReactGA from "react-ga";
-import { useEffect } from "react";
 
-function initializeGA() {
-  ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`);
-  ReactGA.pageview("/Homepage");
-}
 function App() {
-  useEffect(() => {
-    initializeGA();
-  }, []);
-
   return (
     <div className="App">
       <Navigation />
